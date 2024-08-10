76e33eac68f854059843ede9b57d31105176e4d9
shrabon <getshrabon@gmail.com>
2024-08-10 17:24:04 +0600

    Updated ARRAY.PHP file

--- array.php
+++ array.php
@@ -64,12 +64,60 @@ $foods = [
 // How to access the foods array all item with loop?
 // Use foreach loop
 
-foreach($foods as $key=>$value){
+foreach ($foods as $key => $value) {
     // echo $key . "=" . $value;
 }
 
 // ================== String to covert array with built in function explode() ==================
 
-$foodNames = explode(", ", "nodouls, tea, rice, briryani"); 
+$foodNames = explode(", ", "nodouls, tea, rice, briryani");
 // var_dump($foodNames);
 // echo $foodNames[2];
+
+
+// ============= Associative Array to convert json data in PHP ============
+
+$asHuman = [
+    'fname' => "Akash",
+    'lname' => "Sahal",
+    'profession' => 'Developer',
+    'age' => 26,
+    'status' => 'mingle'
+];
+
+$akashJson = json_encode($asHuman);
+
+// echo $akashJson;
+
+
+// >>>>>>>> Remove data an Associative Array with unset() built-in function
+
+$hePerson = ['fname' => 'Kuddus', 'lname' => 'Ali'];
+// print_r($hePerson);
+unset($hePerson['lname']); // removed the lname key and value
+// print_r($hePerson);
+
+
+// >>>>>>>>>>>> Array slice
+
+$fruits = ['mango', 'banana', 'orange', 'jackfruit', 'apple'];
+$someFruits = array_slice($fruits, 2, 3);
+$negFruits = array_slice($fruits, 2, -1);
+// print_r($someFruits) ;
+// print_r($negFruits) ;
+
+// >>>>>>>>>>>> Array splice
+$newFruits = ["Maranda", "Grape","Pineapple"];
+$someFruit = array_splice($fruits, 1, 2, $newFruits);
+// print_r($someFruit);
+// print_r($fruits);
+
+// >>>>>>>>>>>>>> Array merge
+$r1 = [22,33,55,66];
+$r2 = [43,55,33,22];
+$merge = array_merge($fruits, $r1,$r2);
+// print_r($merge);
+
+// >>>>>>>>>>>>> PHP sort
+sort($merge);
+// print_r($merge);
